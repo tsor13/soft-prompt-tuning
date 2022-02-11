c0ddcc8b719db08fdf929f92ffea2f4bbed3fc53
tsor13 <tsor1313@gmail.com>
2022-02-11 15:42:27 -0700

    works

---
+++ imdb.py
@@ -0,0 +1,137 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from transformers import AdamW
+from datasets import load_dataset
+
+device = 'cuda'
+
+# load imdb
+dataset = load_dataset('imdb')
+
+# tokenizer
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+
+
+# preprocessing
+def preprocess_function(examples):
+    return tokenizer(examples['text'], truncation=True, max_length=512)
+def label_function(examples):
+    target = 'positive' if examples['label'] == 1 else 'negative'
+    # target = 'bear' if examples['label'] == 1 else 'apple'
+    # randomly choose the target
+    # target = 'positive' if torch.rand(1) > 0.5 else 'negative'
+    output = tokenizer(target, return_tensors='pt')
+    return {'target': target, 'target_input_ids': output['input_ids'], 'target_attention_mask': output['attention_mask']}
+
+train_dataset = dataset['test'].map(preprocess_function, batched=False)
+train_dataset = train_dataset.map(label_function, batched=False)
+
+
+from soft_embedding import SoftEmbedding
+
+# model
+n_tokens = 10
+initialize_from_vocab = True
+
+model = GPT2LMHeadModel.from_pretrained('gpt2').to(device)
+
+s_wte = SoftEmbedding(model.get_input_embeddings(), 
+                      # n_tokens=n_tokens, 
+                      prepend_tokens=n_tokens, 
+                      append_tokens=n_tokens,
+                      initialize_from_vocab=initialize_from_vocab)
+
+# loss function
+loss_function = torch.nn.CrossEntropyLoss(ignore_index=-1)
+
+# data loader
+batch_size = 4
+# make collocator function that fills in the correct padding
+def collocator(batch):
+    # for input_ids, pad with tokenizer.pad_token
+    # get max length of input_ids
+    max_length = max([len(x['input_ids']) for x in batch])
+    # pad with pad_token
+    pad_token = tokenizer.pad_token_id
+    input_ids = torch.full((batch_size, max_length), pad_token).long()
+    # fill in with batch['input_ids']
+    for i, x in enumerate(batch):
+        input_ids[i, :len(x['input_ids'])] = torch.Tensor(x['input_ids']).long()
+    
+    # do same for attention_mask, but pad with 0
+    attention_mask = torch.full((batch_size, max_length), 0).long()
+    for i, x in enumerate(batch):
+        attention_mask[i, :len(x['input_ids'])] = torch.Tensor(x['attention_mask']).long()
+    
+    # do same for target_input_ids, but pad with pad_token
+    max_target_len = max([len(x['target_input_ids']) for x in batch])
+    target_input_ids = torch.full((batch_size, max_target_len), pad_token).long()
+    for i, x in enumerate(batch):
+        target_input_ids[i, :len(x['target_input_ids'])] = torch.Tensor(x['target_input_ids']).long()
+    
+    # do same for target_attention_mask, but pad with 0
+    target_attention_mask = torch.full((batch_size, max_target_len), 0).long()
+    for i, x in enumerate(batch):
+        target_attention_mask[i, :len(x['target_input_ids'])] = torch.Tensor(x['target_attention_mask']).long()
+    
+
+    return {'input_ids': input_ids, 'attention_mask': attention_mask, 'target_input_ids': target_input_ids, 'target_attention_mask': target_attention_mask}
+
+# train_dataset = dataset['train']
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collocator)
+# optimizer over s_wte
+optimizer = AdamW(s_wte.parameters(), lr=3e-4)
+for batch in train_dataloader:
+    # get batch
+    batch = {k: v.to(device) for k, v in batch.items()}
+    batch['target_present'] = True
+    embeds = s_wte(batch)
+    # forward pass
+    output = model(inputs_embeds=embeds['inputs_embeds'].to(device), attention_mask=embeds['attention_mask'].to(device))
+    # get logits
+    logits = output['logits']
+    # do cross entropy loss
+    logit_flat = logits.view(-1, logits.size(-1))
+    labels_flat = embeds['labels'].view(-1).to(device)
+    loss = loss_function(logit_flat, labels_flat)
+    # backprop
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    # get accuracy
+    _, preds = torch.max(logit_flat, 1)
+    # see if matches
+    match = (preds == labels_flat).float()
+    # only keep where labels_flat != -1
+    match = match[labels_flat != -1]
+    accuracy = match.mean()
+    print(loss)
+    print(accuracy)
+    print(tokenizer.decode(preds[labels_flat != -1]))
+
+# inputs = tokenizer(['Luke, may the force be with', 'Are tacos food?', 'My least favorite food is'], return_tensors="pt", padding=True)
+# outputs = tokenizer(['you', 'yes', 'tuna'], return_tensors="pt", padding=True)
+# inputs['target_input_ids'] = outputs['input_ids']
+# inputs['target_attention_mask'] = outputs['attention_mask']
+# inputs['target_present'] = True
+# test = s_wte(inputs)
+# breakpoint()
+# output = model(inputs_embeds=test['inputs_embeds'], attention_mask=test['attention_mask'])
+# logits = output['logits']
+# # do cross entropy loss
+# logit_flat = logits.view(-1, logits.size(-1))
+# labels_flat = test['labels'].view(-1)
+# loss = loss_function(logit_flat, labels_flat)
+# 
+# 
+# # apply softmax to logits
+# logprobs = torch.nn.functional.log_softmax(logits, dim=-1)
+# 
+# batch_size = logprobs.size(0)
+# # do cross entropy loss where target_mask
+# loss = loss_function(logprobs, outputs['labels'])
+# 
+# pass
--- soft_embedding.py
+++ soft_embedding.py
@@ -4,28 +4,35 @@ import torch.nn as nn
 class SoftEmbedding(nn.Module):
     def __init__(self, 
                 wte: nn.Embedding,
-                n_tokens: int = 10, 
+                prepend_tokens: int = 10, 
+                append_tokens: int = 0, 
                 random_range: float = 0.5,
                 initialize_from_vocab: bool = True):
         """appends learned embedding to 
 
         Args:
             wte (nn.Embedding): original transformer word embedding
-            n_tokens (int, optional): number of tokens for task. Defaults to 10.
+            prepend_tokens (int, optional): number of tokens for task. Defaults to 10.
             random_range (float, optional): range to init embedding (if not initialize from vocab). Defaults to 0.5.
             initialize_from_vocab (bool, optional): initalizes from default vocab. Defaults to True.
         """
         super(SoftEmbedding, self).__init__()
         self.wte = wte
-        self.n_tokens = n_tokens
-        self.learned_embedding = nn.parameter.Parameter(self.initialize_embedding(wte,
-                                                                               n_tokens, 
+        self.prepend_tokens = prepend_tokens
+        self.append_tokens = append_tokens
+        self.prepend_embedding = nn.parameter.Parameter(self.initialize_embedding(wte,
+                                                                               prepend_tokens, 
                                                                                random_range, 
                                                                                initialize_from_vocab))
             
+        if append_tokens > 0:
+            self.append_embedding = nn.parameter.Parameter(self.initialize_embedding(wte,
+                                                                                append_tokens, 
+                                                                                random_range, 
+                                                                                initialize_from_vocab))
     def initialize_embedding(self, 
                              wte: nn.Embedding,
-                             n_tokens: int = 10, 
+                             prepend_tokens: int = 10, 
                              random_range: float = 0.5, 
                              initialize_from_vocab: bool = True):
         """initializes learned embedding
@@ -37,10 +44,10 @@ class SoftEmbedding(nn.Module):
             torch.float: initialized using original schemes
         """
         if initialize_from_vocab:
-            return self.wte.weight[:n_tokens].clone().detach()
-        return torch.FloatTensor(n_tokens, wte.weight.size(1)).uniform_(-random_range, random_range)
+            return self.wte.weight[:prepend_tokens].clone().detach()
+        return torch.FloatTensor(prepend_tokens, wte.weight.size(1)).uniform_(-random_range, random_range)
             
-    def forward(self, tokens):
+    def forward(self, inputs):
         """run forward pass
 
         Args:
@@ -49,6 +56,107 @@ class SoftEmbedding(nn.Module):
         Returns:
             torch.float: encoding of text concatenated with learned task specifc embedding
         """
-        input_embedding = self.wte(tokens[:, self.n_tokens:])
-        learned_embedding = self.learned_embedding.repeat(input_embedding.size(0), 1, 1)
-        return torch.cat([learned_embedding, input_embedding], 1)
+        def insert3d(a, b, where):
+            # insert b into a at index where
+            c = torch.zeros(a.size(0), a.size(1) + b.size(1), a.size(2))
+            for index in range(a.size(0)):
+                split_index = where[index]
+                c[index, :split_index, :] = a[index, :split_index, :]
+                c[index, split_index:split_index+b.size(1), :] = b[index, :, :]
+                c[index, split_index+b.size(1):, :] = a[index, split_index:, :]
+            return c
+        def insert2d(a, b, where):
+            # insert b into a at index where
+            c = torch.zeros(a.size(0), a.size(1) + b.size(1))
+            for index in range(a.size(0)):
+                split_index = where[index]
+                c[index, :split_index] = a[index, :split_index]
+                c[index, split_index:split_index+b.size(1)] = b[index, :]
+                c[index, split_index+b.size(1):] = a[index, split_index:]
+            return c
+
+        tokens, attention_mask = inputs['input_ids'], inputs['attention_mask']
+        device = tokens.device
+        targets = inputs['target_present']
+        if targets:
+            target_tokens, target_attention_mask = inputs['target_input_ids'], inputs['target_attention_mask']
+        # TODO - why???
+        # input_embedding = self.wte(tokens[:, self.prepend_tokens:])
+        input_embedding = self.wte(tokens)
+        prepend_embedding = self.prepend_embedding.repeat(input_embedding.size(0), 1, 1)
+        embedding = torch.cat([prepend_embedding, input_embedding], 1)
+        attention_mask = torch.cat([torch.ones(input_embedding.size(0), self.prepend_tokens).long().to(device), attention_mask], 1)
+        # get index of the last 1 in each row of attention_mask
+        last_one_index = torch.max(attention_mask, 1)[1]
+        # first zero index is one more
+        split = last_one_index + 1
+        # if targets, insert at last_one_index + 1
+        if targets:
+            # target_tokens, target_attention_mask = targets['input_ids'], targets['attention_mask']
+            target_embedding = self.wte(target_tokens)
+            # insert
+            embedding = insert3d(embedding, target_embedding, split)
+            attention_mask = insert2d(attention_mask, target_attention_mask, split)
+        if self.append_embedding is not None:
+            # insert
+            append_embedding = self.append_embedding.repeat(input_embedding.size(0), 1, 1)
+            embedding = insert3d(embedding, append_embedding, split)
+            attention_mask = insert2d(attention_mask, torch.ones(input_embedding.size(0), self.append_tokens).long().to(device), split)
+        # make a target mask that is zeros everywhere except for the target's attention mask
+        target_mask = torch.zeros(attention_mask.size(0), attention_mask.size(1))
+        for index, split_ind in enumerate(split):
+            # add append tokens so we get to the original index where target starts
+            split_ind += self.append_tokens
+            if targets:
+                target_size = target_attention_mask.size(1)
+                target_mask[index, split_ind:split_ind+target_size] = target_attention_mask[index]
+            else:
+                target_mask[index, split_ind] = 1
+        # make lables, eos tokenizer everywhere except for the target
+        labels = torch.ones(attention_mask.size(0), attention_mask.size(1)).long().to(device) * -1
+        for index, split_ind in enumerate(split):
+            target_len = target_attention_mask.size(1)
+            inds = torch.where(target_mask[index] == 1)[0]
+            target_inds = inds - split_ind
+            # and set
+            labels[index, inds] = target_tokens[index, target_inds]
+
+        # TODO - correct?
+        # to fix off by one error for targets
+        new_target_mask = torch.zeros(target_mask.size(0), target_mask.size(1)).long().to(device)
+        new_target_mask[:, 1:] = target_mask[:, :-1]
+        target_mask = new_target_mask
+        # same for target_tokens
+        new_target_tokens = torch.zeros(target_tokens.size(0), target_tokens.size(1)).long().to(device)
+        new_target_tokens[:, :-1] = target_tokens[:, 1:]
+        # TODO - EOS?
+        new_target_tokens[:, -1] = -1
+        target_tokens = new_target_tokens
+        # and labels
+        new_labels = torch.zeros(labels.size(0), labels.size(1)).long().to(device)
+        new_labels[:, :-1] = labels[:, 1:]
+        new_labels[:, -1] = -1
+        labels = new_labels
+
+
+
+        if targets:
+            inputs = {
+                'inputs_embeds': embedding,
+                'attention_mask': attention_mask,
+                'target_mask': target_mask,
+                # 'target_ids': targets['input_ids'],
+                'target_ids': target_tokens,
+                'split': split,
+                'labels': labels,
+            }
+        else:
+            inputs = {
+                'inputs_embeds': embedding,
+                'attention_mask': attention_mask,
+                'target_mask': target_mask,
+                'split': split,
+                'labels': labels,
+            }
+        # return torch.cat([prepend_embedding, input_embedding], 1)
+        return inputs
